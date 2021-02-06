e94efb410cf03dd372f4bc239dba61cbb8a143aa
Jonathan Fischer <skelothan@gmail.com>
2021-02-06 17:18:38 -0500

    Child relationships added

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,5 @@
 class Child < ApplicationRecord
-    
+    # Relationships
+    has_many :chores
+    has_many :tasks, through: :chores
 end
--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,2 +1,5 @@
 class Chore < ApplicationRecord
+    # Relationships
+    belongs_to :child
+    has_many :tasks
 end
