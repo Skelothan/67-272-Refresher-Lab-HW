88fabd383490ddccba742d76b5bf5f6075f89589
Jonathan Fischer <skelothan@gmail.com>
2021-02-06 17:55:45 -0500

    Chore model done, all tests pass

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,5 +1,22 @@
 class Chore < ApplicationRecord
     # Relationships
     belongs_to :child
-    belongs_to :tasks
+    belongs_to :task
+
+    # Validations
+    validates_date :due_on
+
+    # Scopes
+    scope :by_task,             lambda{joins("JOIN tasks ON tasks.id = chores.task_id").order("tasks.name")}
+    scope :chronological,       lambda{order(:due_on, :completed)}
+    scope :pending,             lambda{where(completed: false)}
+    scope :done,                lambda{where(completed: true)}
+    scope :upcoming,            lambda{where("due_on >= ?", Date.today)}
+    scope :past,                lambda{where("due_on < ?", Date.today)}
+
+    # Methods
+    def status
+        return completed ? "Completed" : "Pending"
+    end
+
 end
